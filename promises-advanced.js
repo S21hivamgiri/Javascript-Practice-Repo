af9ae7eea712e2934d627ec4ec383d612a76c1c4
Shivam Giri <shivamgiri2015@gmail.com>
2026-08-10 08:45:30 +0200

    Debounce Time

--- promises-advanced.js
+++ promises-advanced.js
@@ -1,18 +1,37 @@
+// 4. Debounce Timer
+const debounceTimer = (callback, delay) => {
+  let timerId;
+  return (...args)=>{
+    clearTimeout(timerId)
+    timerId= setTimeout(callback(...args),delay)
+  }
+};
+const debounce = debounceTimer((data) => console.log(data), 2000);
+debounce("Hello, World!");
+setTimeout(() => {
+  debounce("Hello, World! 1");
+}, 1000);
+setTimeout(() => {
+  debounce("Hello, World! 2");
+}, 3500);
+debounce("Hello, World! 3");
+
+
 //3. Timeout with retries
 const promiseTimeoutWithRetries = (callback, timeout, retryLimit) => {
-    return callback().catch((err) => {
-      if (retryLimit <= 0) throw err;
-       console.log(`Retrying... ${retryLimit  - 1} attempts left`);
-      return new Promise((resolve, reject) =>
-        setTimeout(
-          () =>
-            promiseTimeoutWithRetries(callback, timeout, retryLimit - 1)
-              .then(resolve)
-              .catch(reject),
-          timeout,
-        ),
-      );
-    });
+  return callback().catch((err) => {
+    if (retryLimit <= 0) throw err;
+    console.log(`Retrying... ${retryLimit - 1} attempts left`);
+    return new Promise((resolve, reject) =>
+      setTimeout(
+        () =>
+          promiseTimeoutWithRetries(callback, timeout, retryLimit - 1)
+            .then(resolve)
+            .catch(reject),
+        timeout,
+      ),
+    );
+  });
 }
 promiseTimeoutWithRetries(unResolvedApi, 1000, 3)
   .then((data) => console.log(data))
@@ -26,25 +45,25 @@ const promiseTimeout = (promise, timeout) => {
   return Promise.race([promise, promise2]);
 };
 
-// promiseTimeout(resolvedDelayedApi(2000), 1000)
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error("Failed:", err));
+promiseTimeout(resolvedDelayedApi(2000), 1000)
+  .then((data) => console.log(data))
+  .catch((err) => console.error("Failed:", err));
 
-// // 1.Retries promise n number of promised
-// function retriesPromisesNTimes(callback, n) {
-//   return callback().catch((err) => {
-//     if (err) {
-//       if (n <= 0) throw err;
+// 1.Retries promise n number of promised
+function retriesPromisesNTimes(callback, n) {
+  return callback().catch((err) => {
+    if (err) {
+      if (n <= 0) throw err;
 
-//       console.log(`Retrying ${n} times... ${n - 1} attempts left`);
-//       return retriesPromisesNTimes(callback, n - 1);
-//     }
-//   });
-// }
+      console.log(`Retrying ${n} times... ${n - 1} attempts left`);
+      return retriesPromisesNTimes(callback, n - 1);
+    }
+  });
+}
 
-// retriesPromisesNTimes(unResolvedApi, 3, 1000)
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error("Failed:", err));
+retriesPromisesNTimes(unResolvedApi, 3, 1000)
+  .then((data) => console.log(data))
+  .catch((err) => console.error("Failed:", err));
 
 // Utilities functions
 function resolvedApi() {
